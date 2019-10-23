9089d8cb6c9e6834d225187bceb9a23ee864222b
Gabriela S. Soria <soria.gaby@gmail.com>
2019-10-23 11:04:50 -0700

    Force to use new site tar
    Signed-off-by: Gabriela S. Soria <soria.gaby@gmail.com>

--- html-css/module-1/env-init.sh
+++ html-css/module-1/env-init.sh
@@ -1,4 +1,4 @@
-curl -L https://www.dropbox.com/s/dlpi7z1xjaiz908/pf-express.tar.gz?dl=0 > ~/pf-express.tar.gz
+curl -L https://www.dropbox.com/s/l34n8wwmbvhcm2k/pf-express-2.tar.gz?dl=0 > ~/pf-express.tar.gz
 tar -xvzf ~/pf-express.tar.gz -C ~/tutorial
 cd ~/tutorial
 npm install
