0e80472671c87dc497141bdb8d2b536ea78601b2
Gabriela S. Soria <soria.gaby@gmail.com>
2019-12-05 21:11:58 -0800

    Html scenarios using the updated file
    Signed-off-by: Gabriela S. Soria <soria.gaby@gmail.com>

--- html-css/modifier-utilities/env-init.sh
+++ html-css/modifier-utilities/env-init.sh
@@ -1,4 +1,4 @@
-curl -L https://raw.githubusercontent.com/patternfly/training-scenarios/master/site/pf-express.tar.gz > ~/pf-express.tar.gz
+curl -L https://raw.githubusercontent.com/gsoria/training-scenarios/gs_call_event_service/site/pf-express.tar.gz  > ~/pf-express.tar.gz
 tar -xvzf ~/pf-express.tar.gz -C ~/tutorial
 cd ~/tutorial
 npm install
